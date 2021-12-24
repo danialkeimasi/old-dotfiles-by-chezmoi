1676ee194c6460e8971c64a8b36a416cfc215848
Danial Keimasi <danialkeimasi@gmail.com>
2021-12-24 16:59:10 +0330

    Add confirmation to scripts to skip them

--- scripts/run_once_poetry.sh
+++ scripts/run_once_poetry.sh
@@ -1,1 +1,5 @@
+#!/bin/sh
+echo "Installing poetry ..."
+read -p "Continue? [y/N]: " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 0
+
 curl -sSL https://install.python-poetry.org | python3 -
