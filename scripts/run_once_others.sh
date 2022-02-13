988b00e47fba23ac77f0e71e7bbe49ea7d999af3
Danial Keimasi <danialkeimasi@gmail.com>
2022-02-13 17:50:21 +0330

    Install zsh-syntax-highlighting

--- scripts/run_once_others.sh
+++ scripts/run_once_others.sh
@@ -8,3 +8,6 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 echo "Installing vim plugins using vundle ..."
 vim +PluginInstall +qall
+
+echo "Installing zsh plugins ..."
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
